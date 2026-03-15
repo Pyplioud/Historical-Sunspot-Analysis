d82e79c2b5e41ba26553ef8dc117c351158e5b5d
Pyplioud <andreiryuji@gmail.com>
2026-03-14 21:23:53 -0300

    Adds the code behind the analysis

---
+++ sunspot/sunspots.py
@@ -0,0 +1,20 @@
+import csv
+import matplotlib.pyplot as plt
+
+x = 1
+years = []
+spots = []
+
+with open("ISSN_M_tot.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        years.append(float(row["year"])) 
+        spots.append(float(row["total"]))
+plt.style.use('bmh')
+ax = plt.gca()
+ax.set_facecolor("#131D55FF")
+plt.plot(years, spots, color="#E27A19FF")
+plt.title("Number of sunspots x Years")
+plt.xlabel("Year")
+plt.ylabel("Number of Sunspots")
+plt.show()
